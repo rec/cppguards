7164f2af44981780e0b726940444dc1445c925e9
Tom Ritchford <tom@swirly.com>
2015-01-29 16:33:31 -0500

    Add argument parsing.

--- cppguards.py
+++ cppguards.py
@@ -2,12 +2,61 @@
 
 from __future__ import absolute_import, division, print_function, unicode_literals
 
+import argparse
 import os
 import sys
 
-ROOT_NAMES = 'src', 'source'
-SKIPPED_NAMES = 'test', 'tests', 'impl'
-SUFFIX = 'H_INCLUDED'
+NAME = 'cppguards'
+VERSION = '1.0'
+
+PARSER = argparse.ArgumentParser(
+    prog='cppguards',
+    description='Add #ifdef guards to C/C++ header files.',
+    )
+
+# Positional arguments.
+PARSER.add_argument(
+    'files',
+    nargs='+',
+    help='Files to clean.'
+)
+
+# Flag arguments
+
+PARSER.add_argument(
+    '--continue',
+    action='store_true',
+    help="Keep going even if there's an error in one file.",
+)
+
+PARSER.add_argument(
+    '--noexecution', '-n',
+    action='store_true',
+    help='If set, print the guards without editing the files.',
+)
+
+PARSER.add_argument(
+    '--roots',
+    default='src:source',
+    help='Names of directories that might be the root of the source tree.',
+)
+
+PARSER.add_argument(
+    '--skip',
+    default='test:tests:impl',
+    help='Names of directories that should be skipped in forming a guard.',
+)
+
+PARSER.add_argument(
+    '--suffix',
+    default='H_INCLUDED',
+    help='Suffix added to the end of the guard.',
+)
+
+ARGS = PARSER.parse_args()
+
+ROOT_NAMES = ARGS.roots.split(':')
+SKIPPED_NAMES = ARGS.skip.split(':')
 
 def _find_root(fname):
     path = []
@@ -22,15 +71,25 @@ def _find_root(fname):
         fname = head
         path.insert(0, tail)
 
+def _remove_extensions(fname):
+    extensions = []
+    while True:
+        fname, ext = os.path.splitext(fname)
+        if ext:
+            extensions.insert(0, ext)
+        else:
+            return fname, extensions
+
 def get_guard(fname):
     fname = os.path.abspath(fname)
     root, path = _find_root(fname)
     path = [p for p in path if p not in SKIPPED_NAMES]
-    path[-1], extension = os.path.splitext(path[-1])
-    assert extension == '.h'
 
-    path.append(SUFFIX)
-    return '_'.join(p.upper() for p in path)
+    path[-1], extensions = _remove_extensions(path[-1])
+    assert extensions[-1] == '.h'
+
+    path.append(ARGS.suffix)
+    return '_'.join(p.upper() for p in path).replace('-', '_')
 
 def add_guards(fname):
     guard = get_guard(fname)
@@ -38,22 +97,33 @@ def add_guards(fname):
     contents = open(fname, 'r').readlines()
 
     for i, line in enumerate(contents):
-        if line.startswith('#ifndef'):
-            count = 2 if contents[i + 1].startswith('#define') else 1
-            if contents[i + count].isspace():
-                count += 1
-            contents[i:i + count] = guard_lines
-            break
-        if line.startswith('#'):
-            contents[i:i] = guard_lines
-            contents.append('#endif\n')
-            break
+        try:
+            if line.startswith('#ifndef'):
+                count = 2 if contents[i + 1].startswith('#define') else 1
+                if contents[i + count].isspace():
+                    count += 1
+                contents[i:i + count] = guard_lines
+                break
+            if line.startswith('#'):
+                contents[i:i] = guard_lines
+                contents.append('#endif\n')
+                break
+        except UnicodeDecodeError as e:
+            e.reason += ' on line %d' % (1 + i)
+            raise e
 
     open(fname, 'w').writelines(contents)
 
 if __name__ == '__main__':
-    args = sys.argv[1:]
-    if not args:
-        raise Exception('Usage: cppguards <file> [<file> ...]')
-    for f in args:
-        add_guards(f)
+    for f in ARGS.files:
+        if ARGS.noexecution:
+            print('%s: %s' % (f, get_guard(f)))
+        else:
+            try:
+                add_guards(f)
+            except Exception as e:
+                e.reason += ' in file "%s".' % f
+                if getattr(ARGS, 'continue'):
+                    print('ERROR:', e.reason)
+                else:
+                    raise
